a97b1edbbf16ea1d3eaff4758fff6aeb4a81319d
Luna Seifert <thepuddingdeity@gmail.com>
2023-05-09 19:51:03 -0500

    Added first two routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/trainers" do
+    Trainer.all.to_json(include: {pets: {only: :id}})
+  end
+
+  get "/pets" do
+    Pet.all.to_json
   end
 
 end
