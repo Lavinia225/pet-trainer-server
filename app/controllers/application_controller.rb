b3fdef47271a13d924dd5acb1eed62722d51b453
Luna Seifert <thepuddingdeity@gmail.com>
2023-05-17 18:28:57 -0500

    Removed useless route, told original GET route to include all data instead of being selective

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   get "/trainers" do
-    Trainer.all.to_json(include: {pets: {only: :id}})
-  end
-
-  get "/pets" do
-    Pet.all.to_json
+    Trainer.all.to_json
   end
 
   post "/trainers" do
--- db/migrate/20230508213557_create_pets.rb
+++ db/migrate/20230508213557_create_pets.rb
@@ -1,4 +1,4 @@
-class CreatePets < ActiveRecord::Migration[6.1] #name, Species, breed, Age, Gender, personality, trainer, working on
+class CreatePets < ActiveRecord::Migration[6.1]
   def change
     create_table :pets do |t|
       t.string :name
