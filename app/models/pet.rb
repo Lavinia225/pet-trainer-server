1ff85f48914c70bb0dfdc0030e0a773b3ed2b3a9
Luna Seifert <thepuddingdeity@gmail.com>
2023-05-26 17:41:01 -0500

    Added two class methods simply because I can.

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 class Pet < ActiveRecord::Base
     belongs_to :trainer
+
+    def self.species_count
+        total_species_counts = ""
+        petArray = Pet.select(:species).distinct
+        species_array = petArray.map do |pet|
+                pet.species
+            end
+    
+        species_array.each do |species|
+            count = Pet.where("species = '#{species}'").count
+            total_species_counts += "#{species}: #{count} "
+        end
+        total_species_counts
+    end
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,3 +1,7 @@
 class Trainer < ActiveRecord::Base
     has_many :pets
+
+    def self.sum_pay
+        Trainer.pluck(:payrate).sum
+    end
 end
